dee189a864a1c2a1d9af78b3b5f75bfa95c6341e
Al Amin <alamin.mistrisolutions@gmail.com>
2021-10-29 09:45:22 +0600

    option api event with print value

---
+++ larafrontend/components/optionsevent.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" @input="optionApiEvent" class="h-10 w-full rounded-md border border-gray-200 px-2">
+</template>
+
+<script>
+export default {
+  name: "optionsEvent",
+  methods:{
+    optionApiEvent(event){
+      this.$emit('optionEvent' , event.target.value)
+    }
+
+  }
+}
+</script>
--- larafrontend/pages/dashboard/customEvent.vue
+++ larafrontend/pages/dashboard/customEvent.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div class="space-y-4">
     <div class="px-5 py-10 bg-gray-100 rounded-md">
       <h1>Inline Event: {{ inlineValue }} </h1>
-      <inlineevent placeholder="inline event" @inlineEvent="eventTriggerFromParent"/>
+      <inlineevent placeholder="inline event" @inlineEvent="inlineEventTriggerFromParent"/>
+    </div>
+    <div class="px-5 py-10 bg-blue-100 rounded-md">
+      <h1>Option Api Event: {{ optionValue }} </h1>
+      <optionsevent placeholder="option api event" @optionEvent="optionEventTriggerFromParent"/>
     </div>
   </div>
 </template>
@@ -12,12 +16,17 @@ export default {
   name: "customEvent",
   data () {
     return {
-      inlineValue: ''
+      inlineValue: '',
+      optionValue: ''
     }
   },
   methods: {
-    eventTriggerFromParent(event){
+    inlineEventTriggerFromParent(event){
       this.inlineValue = event
+      console.log(event)
+    },
+    optionEventTriggerFromParent(event){
+      this.optionValue = event
       console.log(event)
     }
   }
