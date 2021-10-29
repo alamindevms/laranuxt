a8faac90fd963fb12270e6b55bbf9bf9493c7bf8
Al Amin <alamin.mistrisolutions@gmail.com>
2021-10-29 18:13:51 +0600

    .sync modifier

---
+++ larafrontend/components/modelmodifier.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    type="text"
+    :value="text"
+    @input="onInput"
+    class="h-10 w-full rounded-md border border-gray-200 px-2"
+  >
+</template>
+
+<script>
+export default {
+  name: "modelModifier",
+  props:{
+    text: String,
+  },
+  methods:{
+    onInput(event){
+      this.$emit('update:text' , event.target.value)
+    }
+  }
+}
+</script>
--- larafrontend/pages/dashboard/twoWayDataBinding.vue
+++ larafrontend/pages/dashboard/twoWayDataBinding.vue
@@ -4,6 +4,21 @@
       <h1>Two Way Data Binding: {{ value }} </h1>
       <twowaydatabinding :modelValue="value" @update:modelValue="value = $event" placeholder="Data Binding"/>
     </div>
+
+    <div class="px-5 py-10 bg-gray-100 rounded-md">
+      <h1>Two Way Data Binding with Modifier: {{ text }} </h1>
+      <modelmodifier :text.sync="text" placeholder="Data Binding"/>
+
+      <div class="bg-gray-50 rounded-md p-2 mt-4">
+        <p class="text-base font-medium text-gray-800">The ".sync" modifier (VueJS 2)</p>
+        <p class="text-sx font-light text-gray-600">This is not a v-model implementation exactly but it will work as it is. With the .sync modifier (VueJS 2.3+), the child component doesn’t need a value prop. Instead, it uses the same prop name you synced in the parent.
+          Also instead of emitting an input event to update the prop, you emit the conveniently named event update:text. (Source:
+          <a href="https://medium.com/front-end-weekly/vues-v-model-directive-vs-sync-modifier-d1f83957c57c" class="text-blue-600">Vue’s new and improved prop.sync</a>).</p>
+
+      </div>
+    </div>
+
+    <p class="text-sx font-light">Docs: <a href="https://dev.to/vcpablo/vuejs-2-different-ways-to-implement-v-model-1mjf" class="text-blue-600 my-2">https://dev.to/vcpablo/vuejs-2-different-ways-to-implement-v-model-1mjf</a></p>
   </div>
 </template>
 
@@ -12,7 +27,8 @@ export default {
   name: "twoWayDataBinding",
   data(){
     return {
-      value: ''
+      value: '',
+      text: ''
     }
   },
 }
